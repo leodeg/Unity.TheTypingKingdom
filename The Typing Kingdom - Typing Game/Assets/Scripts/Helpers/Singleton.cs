dc16f4e1726e0e7304b5b15254ef4925760344c8
LeoDeg <leodeg.dev@gmail.com>
2021-03-02 21:37:50 +0300

    refactor: added singleton generic class

---
+++ The Typing Kingdom - Typing Game/Assets/Scripts/Helpers/Singleton.cs
@@ -0,0 +1,27 @@
+﻿
+public class Singleton<T> where T : new()
+{
+	public static T instance { get; set; }
+
+	private static object locker = new object();
+
+	public static T Instance
+	{
+		set
+		{
+			lock (locker)
+			{
+				instance = value;
+			}
+		}
+		get
+		{
+			lock (locker)
+			{
+				if (instance == null)
+					instance = new T();
+				return instance;
+			}
+		}
+	}
+}
--- The Typing Kingdom - Typing Game/Assets/Scripts/Models/GameData.cs
+++ The Typing Kingdom - Typing Game/Assets/Scripts/Models/GameData.cs
@@ -1,30 +1,5 @@
 ﻿[System.Serializable]
-public class GameData
+public class GameData : Singleton<GameData>
 {
-	private static GameData instance;
-	private static object locker = new object();
-
-	public static GameData Instance
-	{
-		set
-		{
-			lock (locker)
-			{
-				//if (value == null)
-				//	instance = new SaveData();
-				instance = value;
-			}
-		}
-		get
-		{
-			lock (locker)
-			{
-				if (instance == null)
-					instance = new GameData();
-				return instance;
-			}
-		}
-	}
-
 	public PlayerProfilesContainer PlayerProfiles { get; set; } = new PlayerProfilesContainer();
 }
